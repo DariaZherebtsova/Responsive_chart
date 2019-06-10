7e53769f3aa691b4973a769e5f6834af25c0bfc4
Daria Zherebtsova <dd_mail@inbox.ru>
2019-06-11 00:32:37 +0300

    change file organization

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var staticSiteOptions = {
 
 // Запуск сайта:
 express().use(express.static(
-   path.join(__dirname, 'public'),
+   path.join(__dirname, '/'),
    staticSiteOptions
 )).listen(staticSiteOptions.portnum);
 
